681c494b3223d5b65da45b211ab8f9c5a797003b
Aboje Edwin <abojeedwin@gmail.com>
2026-01-20 16:18:19 +0100

    Second commit

---
+++ web-api/app/consumer-http-basic.js
@@ -0,0 +1,26 @@
+const server = require('fastify')();
+const fetch= require('node-fetch');
+const HOST = process.env.HOST || '127.0.0.1'
+const PORT = process.env.PORT || 3000;
+const TARGET =process.env.TARGET || 'localhost:4000';
+const error = require('fastify-error');
+
+server.get('/', async () => {
+    try{
+        const res = await fetch(`http://${TARGET}/recipes/42`);
+        const producer_data = await res.json();
+        return {
+            consumer_pid: process.pid,
+            producer_data
+        };
+    }catch(err){
+        return {
+            error: err,
+            code: err.status,
+            message: 'Error fetching producer data.'
+        }
+    }
+});
+server.listen(PORT, HOST, () => {
+    console.log(`Consumer running at http://${HOST}:${PORT}/`);
+});
